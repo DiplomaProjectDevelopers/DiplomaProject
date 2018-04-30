fb67d9cac30836449fe797b14746070948164169
Hakob Papazyan <hakobpapazyan2@gmail.com>
2018-05-01 00:30:57 +0400

    TemporaryCommit

---
+++ DiplomaProject.Domain/Entities/UserProfessions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiplomaProject.Domain.Entities
+{
+    public class UserProfessions
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ProfessionId { get; set; }
+
+        public User User { get; set; }
+
+        public Profession Profession { get; set; }
+    }
+}

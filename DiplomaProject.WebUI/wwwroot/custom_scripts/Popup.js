f055722e2a4d378dd53e75fd4c4474d0d8427311
KimSargsian <36208263+KimSargsian@users.noreply.github.com>
2018-05-16 02:00:54 +0400

    com

--- DiplomaProject.WebUI/wwwroot/custom_scripts/Popup.js
+++ DiplomaProject.WebUI/wwwroot/custom_scripts/Popup.js
@@ -1,15 +1,9 @@
-﻿<script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            //Скрыть PopUp при загрузке страницы    
-            PopUpHide();
-        });
-    //Функция отображения PopUp
-    function PopUpShow(){
-            $("#popup1").show();
-        }
-    //Функция скрытия PopUp
-    function PopUpHide(){
-            $("#popup1").hide();
-        }
-</script>
+﻿$(document).ready(function () {
+    PopUpHide();
+});
+function PopUpShow() {
+    $("#popup1").show();
+}
+function PopUpHide() {
+    $("#popup1").hide();
+}
